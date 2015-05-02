fb9a1a51993c3b963016f49287d4c2d8913f2c92
Daniel Davidson <dbdavidson@yahoo.com>
2015-05-02 05:11:52 -0500

    in process of promoting Entity from ebisu_cpp to ebisu

---
+++ lib/src/ebisu/entity.dart
@@ -0,0 +1,106 @@
+/// Provides support for mixing in recursive design pattern
+part of ebisu.ebisu;
+
+abstract class Identifiable {
+
+  // custom <class Identifiable>
+
+  Id get id;
+
+  // end <class Identifiable>
+
+}
+
+/// Provides support for mixing in recursive design pattern among various
+/// *Entities*
+///
+abstract class Entity implements Identifiable {
+
+  /// Description of entity
+  String descr;
+  /// The entity containing this entity (e.g. the [Class] containing the [Member]).
+  /// [Installation] is a top level entity and has no owner.
+  Entity get owner => _owner;
+  /// Path from root to this entity
+  List<Entity> get entityPath => _entityPath;
+
+  // custom <class Entity>
+
+  /// All entities must provide iteration on [children] entities
+  Iterable<Entity> get children;
+
+  String get briefComment => brief != null ? '//! $brief' : null;
+
+  String get detailedComment => descr != null ? blockComment(descr, ' ') : null;
+
+  String get docComment => combine([briefComment, detailedComment]);
+
+  Iterable<Id> get entityPathIds => _entityPath.map((e) => e.id);
+
+  get uniqueId => entityPathIds.toString().hashCode;
+
+  get dottedName => entityPathIds.map((id) => id.snake).join('.');
+
+  get detailedPath => brCompact(
+      entityPath.map((e) => '(${e.runtimeType}:${e.id.snake})').join(', '));
+
+  set doc(String d) => descr = d;
+  get doc => descr;
+
+  get hasComment => brief != null || descr != null;
+
+  /// Establishes the [Entity] that *this* [Entity] is owned by.
+  set owner(Entity newOwner) {
+    bool isRoot = newOwner == null;
+    _owner = newOwner;
+    onOwnershipEstablished();
+
+    if (!isRoot) {
+      _entityPath
+        ..addAll(newOwner.entityPath)
+        ..add(this);
+    }
+
+    _logger.info('Set owner ($id:${runtimeType}) to '
+        '${newOwner == null? "root" : "(${newOwner.id}:${newOwner.runtimeType})"}');
+
+    for (final child in children) {
+      child.owner = this;
+    }
+  }
+
+  /// Called when an owner is assigned to the [Entity]
+  void onOwnershipEstablished() {}
+
+  /// Returns a list of all children recursively
+  List<Entity> get progeny => children.fold([], (all, child) => all
+    ..add(child)
+    ..addAll(child.progeny));
+
+
+  findAncestorWhere(predicate) => predicate(this)
+      ? this
+      : owner == null ? owner : owner.findAncestorWhere(predicate);
+
+  /// Walks the list up throuh the ownership chain, returning a List<Entity> of
+  /// all owners
+  List<Entity> get ancestry {
+    final List<Entity> result = [];
+    var parent = _owner;
+    while (parent != null) {
+      result.add(parent);
+      parent = parent._owner;
+    }
+    return result;
+  }
+
+  /// Recursively walks up to root (i.e. Entity with null parent)
+  get root => _owner == null ? this : _owner.root;
+
+  // end <class Entity>
+
+  Entity _owner;
+  List<Entity> _entityPath = [];
+}
+// custom <part entity>
+// end <part entity>
---
+++ test/test_entity.dart
@@ -0,0 +1,161 @@
+library ebisu.test.test_entity;
+
+import 'package:args/args.dart';
+import 'package:ebisu/ebisu.dart';
+import 'package:id/id.dart';
+import 'package:logging/logging.dart';
+import 'package:unittest/unittest.dart';
+// custom <additional imports>
+// end <additional imports>
+
+final _logger = new Logger('test_entity');
+
+class Base {
+  int ownershipCount = 0;
+
+  // custom <class Base>
+
+  ownerSet() {
+    _logger.info('onOwnershipEstablished called ${runtimeType}');
+    ownershipCount++;
+  }
+
+  toString() => '${runtimeType}:ownershipCount $ownershipCount';
+
+  // end <class Base>
+
+}
+
+class RootEntity extends Base with Entity {
+  List<ChildEntity> children = [];
+
+  // custom <class RootEntity>
+
+  final Id _id;
+  RootEntity(id) : _id = makeId(id);
+  get id => _id;
+  onOwnershipEstablished() => ownerSet();
+
+  // end <class RootEntity>
+
+}
+
+class ChildEntity extends Base with Entity {
+  List<GrandchildEntity> grandChildren = [];
+
+  // custom <class ChildEntity>
+
+  final Id _id;
+  ChildEntity(id) : _id = makeId(id);
+  get id => _id;
+  Iterable<Entity> get children => grandChildren;
+  onOwnershipEstablished() => ownerSet();
+
+  // end <class ChildEntity>
+
+}
+
+class GrandchildEntity extends Base with Entity {
+  List<GreatGrandchildEntity> greatGrandChildren = [];
+
+  // custom <class GrandchildEntity>
+
+  final Id _id;
+  GrandchildEntity(id) : _id = makeId(id);
+  get id => _id;
+  Iterable<Entity> get children => greatGrandChildren;
+  onOwnershipEstablished() => ownerSet();
+
+  // end <class GrandchildEntity>
+
+}
+
+class GreatGrandchildEntity extends Base with Entity {
+
+  // custom <class GreatGrandchildEntity>
+
+  final Id _id;
+  GreatGrandchildEntity(id) : _id = makeId(id);
+  get id => _id;
+  Iterable<Entity> get children => [];
+  onOwnershipEstablished() => ownerSet();
+
+  // end <class GreatGrandchildEntity>
+
+}
+
+// custom <library test_entity>
+// end <library test_entity>
+
+main([List<String> args]) {
+  Logger.root.onRecord.listen(
+      (LogRecord r) => print("${r.loggerName} [${r.level}]:\t${r.message}"));
+  Logger.root.level = Level.OFF;
+// custom <main>
+
+  group('entity', () {
+    final root = new RootEntity(idFromString('root'))
+      ..children = [
+        new ChildEntity('c1')
+          ..grandChildren = [
+            new GrandchildEntity('gc1')
+              ..greatGrandChildren = [
+                new GreatGrandchildEntity('ggc11'),
+                new GreatGrandchildEntity('ggc12'),
+                new GreatGrandchildEntity('ggc13'),
+              ],
+            new GrandchildEntity('gc2')
+              ..greatGrandChildren = [
+                new GreatGrandchildEntity('ggc21'),
+                new GreatGrandchildEntity('ggc22'),
+                new GreatGrandchildEntity('ggc23'),
+              ],
+          ]
+      ];
+
+    root.owner = null;
+
+    _logger.info(brCompact(root.progeny.map((e) => '${e.runtimeType}::${e.id}')));
+
+    test('progeny finds all', () {
+      expect(root.progeny.length, 9);
+      expect(root.progeny.where((e) => e is GreatGrandchildEntity).length, 6);
+    });
+
+    test('progeny finds grandchild', () {
+      final gcc23 = root.progeny.firstWhere((e) => e.id.toString() == 'ggc23');
+      final gc1 = root.progeny.firstWhere((e) => e.id.toString() == 'gc1');
+      final gc2 = root.progeny.firstWhere((e) => e.id.toString() == 'gc2');
+      expect(gcc23.id, new Id('ggc23'));
+      expect(gcc23.root, root);
+      expect(gcc23.findAncestorWhere((a) => a == gc2), gc2);
+      expect(gcc23.findAncestorWhere((a) => a == gc1), null);
+      expect(gcc23.findAncestorWhere((a) => a == root), root);
+    });
+
+    test('ownershipCount', () {
+      final gcc23 = root.progeny.firstWhere((e) => e.id.toString() == 'ggc23');
+      final gc1 = root.progeny.firstWhere((e) => e.id.toString() == 'gc1');
+      final gc2 = root.progeny.firstWhere((e) => e.id.toString() == 'gc2');
+      expect(gcc23.ownershipCount, 1);
+      expect(gc1.ownershipCount, 1);
+      expect(gc2.ownershipCount, 1);
+    });
+
+    test('entityPathIds, detailedPath', () {
+      final gcc23 = root.progeny.firstWhere((e) => e.id.toString() == 'ggc23');
+
+      final c1Id = idFromString('c1');
+      final gc2Id = idFromString('gc2');
+      final ggc23Id = idFromString('ggc23');
+
+      expect(gcc23.entityPathIds, [ c1Id, gc2Id, ggc23Id ]);
+      expect(chomp(gcc23.detailedPath),
+        '(ChildEntity:c1), (GrandchildEntity:gc2), (GreatGrandchildEntity:ggc23)'
+      );
+    });
+  });
+
+// end <main>
+
+}
