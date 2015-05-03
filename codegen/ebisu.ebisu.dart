b96367f1603a7382eba826211ec8f4a8174311dc
Daniel Davidson <dbdavidson@yahoo.com>
2015-05-02 20:01:06 -0500

    bump version

--- codegen/ebisu.ebisu.dart
+++ codegen/ebisu.ebisu.dart
@@ -1171,7 +1171,7 @@ library/templates, a message like the following will be output:
     ..license = 'boost'
     ..rootPath = _topDir
     ..pubSpec = (pubspec('ebisu')
-        ..version = '0.3.5'
+        ..version = '0.3.6'
         ..doc = '''
 A library that supports code generation of the structure Dart (and potentially
 other languages like D) using a fairly declarative aproach.
