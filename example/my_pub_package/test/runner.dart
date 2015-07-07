a285b1d18806ed4ac0cb61f5381b3992e0210df3
Daniel Davidson <dbdavidson@yahoo.com>
2015-07-07 18:37:56 -0500

    removal of dead code

--- example/my_pub_package/test/runner.dart
+++ example/my_pub_package/test/runner.dart
@@ -2,11 +2,6 @@ import 'package:test/test.dart';
 import 'package:logging/logging.dart';
 import 'test_it.dart' as test_it;
 
-void testCore(Configuration config) {
-  unittestConfiguration = config;
-  main();
-}
-
 main() {
   Logger.root.level = Level.OFF;
   Logger.root.onRecord.listen((LogRecord rec) {
